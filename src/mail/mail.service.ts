fdd0c613fb96488abf53507a0197f6688ff01ef7
dragan039 <dragan039@gmail.com>
2026-06-04 15:17:15 +0200

    Dodato email obavestenje za zahteve

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,7 +6,7 @@ export class MailService {
   private transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT) || 465,
-    secure: true,
+    secure: Number(process.env.SMTP_PORT) === 465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
