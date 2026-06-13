e3ba7f816a0fa80622304fae01a32927a13b55ff
dragan039 <dragan039@gmail.com>
2026-06-14 01:38:42 +0200

    Izmenjeni nazivi sajta

--- src/site-translations/site-translations.module.ts
+++ src/site-translations/site-translations.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { SiteTranslationsController } from './site-translations.controller';
 import { SiteTranslationsService } from './site-translations.service';
 import { PrismaService } from '../prisma.service';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
+  imports: [AuthModule],
   controllers: [SiteTranslationsController],
   providers: [SiteTranslationsService, PrismaService],
 })
